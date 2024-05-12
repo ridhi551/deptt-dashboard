20d0b1fa8e7bc2b227a52eab158b10331736eef1
abhishek dogra <abhidograaa19@gmail.com>
2024-05-12 19:08:12 +0530

    Made some changes in User routes and connect DB.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(process.env.MONGO_URI);
-     console.log(`Mongo Database Connected 😗💕💕💕💕`);
+     console.log(`Database Connected`);
     } catch (error) {
-      console.log(`'😒 MongoDB Error message': ${error.message}`);
+      console.log(` MongoDB Error message: ${error.message}`);
       process.exit();
     } 
   }; 
