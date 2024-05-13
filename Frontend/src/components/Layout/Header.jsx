465c6a3aee6db9ad51b820e74b118309d6387aa6
abhishek dogra <abhidograaa19@gmail.com>
2024-05-13 23:36:38 +0530

    Made some changes in UI.

--- Frontend/src/components/Layout/Header.jsx
+++ Frontend/src/components/Layout/Header.jsx
@@ -2,97 +2,57 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import ProfileModal from "../Home/ProfileModal";
-
+import { Avatar, Navbar } from "flowbite-react";
 const Header = () => {
   const user = useSelector((state) => state.user?.userInfo);
   const [show, setShow] = useState(false);
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+    <div className="px-2 md:px-10 py-6">
+      <Navbar fluid rounded className="relative">
+        <Navbar.Brand href="https://flowbite-react.com">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
+            className="mr-3 h-6 sm:h-9"
+            alt="AcademIQ-Connect Logo"
           />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             AcademIQ-Connect
           </span>
-        </Link>
-
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-user"
-        >
-          <ul className="flex gap-10 text-2xl ">
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          {user ? (
+            <button className="cursor-pointer" onClick={() => setShow(!show)}>
+              <Avatar
+                src={user?.pic}
+                className="size-10 rounded-2xl"
+                alt="Logo"
+              />
+            </button>
+          ) : (
             <li>
-              <Link to="/"
-                href="#"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
+              <Link
+                to="/auth"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Home
+                Login
               </Link>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact
-              </a>
-            </li>
+          )}
 
-
-          {
-            user ? (
-              <button className="cursor-pointer" onClick={()=>setShow(!show)}>
-            <img
-              src={user?.pic}
-              className="size-10 rounded-2xl"
-              alt="Logo"
-             
-            />
-          </button>
-            ):(
-              <li>
-                <Link to="/auth"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Login
-                </Link>
-              </li>
-
-            )
-          }
-
-
-            {show && <ProfileModal isOpenProp={show}/>}
-          </ul>
+          {show && <ProfileModal isOpenProp={show} />}
+          <Navbar.Toggle />
         </div>
-
-      </div>
-
-    </nav>
+        <Navbar.Collapse className="absolute md:relative top-full left-0 bg-white rounded ">
+          <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="#">About</Navbar.Link>
+          <Navbar.Link href="#">Services</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 };
-
 export default Header;
