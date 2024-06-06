8e512227778878289cb2f8ea79f40c1800824724
abhishek dogra <abhidograaa19@gmail.com>
2024-06-06 23:40:38 +0530

    .

--- Backend/controllers/semesterController.js
+++ Backend/controllers/semesterController.js
@@ -1,60 +1,43 @@
 const asyncHandler = require("express-async-handler");
 const SemesterModel = require("../models/fileUploadModel");
-const fs = require('fs');
-
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-
-const { v4: uuidv4 } = require('uuid');
-
-
-
-const s3Client = new S3Client({
+const fs = require("fs");
+const AWS = require("aws-sdk");
+require("aws-sdk/lib/maintenance_mode_message").suppress = true;
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
-
 
 const saveSemester = asyncHandler(async (req, res) => {
   const { name, semester } = req.body;
 
-  const file = req.file; 
-
-  const savedUid = uuidv4();
-  const UploadParams = { 
-    Bucket: "skitii-badgeimages",
-    Key: `check/${savedUid}.pdf`,
+  const file = req.file;
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  const UploadParams = {
     Body: fs.createReadStream(file.path),
     ContentType: file.mimetype,
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: `check/${Date.now()}`,
   };
 
-
   try {
-    
-    await s3Client.send(new PutObjectCommand(UploadParams));
+    const data = await s3.upload(UploadParams).promise();
+    const savedSemester = await SemesterModel.create({
+      name,
+      semester,
+      url: data.Location,
+      createdBy: req.user._id,
+    });
+    return res.status(201).json({ savedSemester });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-
-  const savedSemester = await SemesterModel.create({
-    name, 
-    semester,
-    url:data.Location,
-    createdBy: req.user._id,
-  });
- 
-  if (!savedSemester) {
-    throw new Error
-  }  
-
-
-  res.status(201).json(savedSemester ,data.Location);
+  res.status(500).send("Unable to upload the file");
 });
 
 module.exports = {
   saveSemester,
 };
-
--- Backend/routes/TeacherRoutes.js
+++ Backend/routes/TeacherRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const { multerUpload, singleUpload } = require("../middleware/multer");
 const { saveSemester } = require("../controllers/semesterController");
+const { protect } = require("../middleware/authorize");
 
 const router = express.Router();
 router
   .route("/uploadAssignment")
-  .post(multerUpload.single("file"), saveSemester);
+  .post(multerUpload.single("file"), protect, saveSemester);
 
 module.exports = router;
